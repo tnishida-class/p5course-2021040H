70484ae6ee5fff838709177c45731dc3eec564a0
2021040H <91928587+2021040H@users.noreply.github.com>
2021-10-21 11:39:19 +0900

    2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -2,10 +2,25 @@
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
-  noStroke();
+  stroke(100);
+
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if( (i + j) % 2 == 1){
+        fill(112, 125, 135);
+      }
+      else{
+        noFill();
+      }
+      rect(size * i, size * j, size, size);
+      fill(255, 0, 0);
+      if( (i + j) % 2 == 1 && j < 3){
+        circle(size * i + 0.5 * size, size * j + 0.5 * size, 0.88 * size);
+      }
+      fill(0, 0, 0);
+      if( (i + j) % 2 == 1 && 4 < j){
+        circle(size * i + 0.5 * size, size * j + 0.5 * size, 0.88 * size);
+      }
     }
   }
 }
