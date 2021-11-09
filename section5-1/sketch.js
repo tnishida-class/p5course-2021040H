78c414f1147473bb87ed5d1d88de9cf45888266e
2021040H <91928587+2021040H@users.noreply.github.com>
2021-11-09 22:31:22 +0900

    5-1

--- section5-1/sketch.js
+++ section5-1/sketch.js
@@ -1,14 +1,24 @@
-// テキスト「関数を使う」
-// 練習問題：このプログラムを改造してEUの旗を描いてみよう
+
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background(34, 22, 184);
   for(let i = 0; i < 12; i++){
-    let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
-    let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    const theta = TWO_PI * i / 12;
+    const x = 100 + cos(theta) * 50;
+    const y = 100 + sin(theta) * 50;
+    noStroke();
+    fill(235, 232, 52);
+    star(x, y, 10);
   }
 }
 
-// ヒント：section5-2 にある star 関数をここにコピーして、 draw 内で ellipse の代わりに使おう
+function star(cx, cy, r){
+  beginShape();
+  for(var i = 0; i < 5; i++){
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+}
--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,18 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 60, 60, 145);
+
 }
 
-function balloon(t){
+function balloon(t, x, y, color){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
+  fill(color);
+  ellipse(x + 60, y + 10, w + p * 2 + 50, h + p * 2 + 40);
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  rect(x, y, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, x + 3, y + 15);
 }
